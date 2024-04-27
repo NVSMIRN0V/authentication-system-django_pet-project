0188bd2bba9f7f88356dfa31b4b59659e5a3c16f
Nikita Smirnov <job.nvsmirnov@gmail.com>
2024-04-27 10:45:43 +0300

    forms added
    Добавил форма для регистрации и входа в аккаунт.

---
+++ users/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from users.models import User
+
+
+class UserSignInForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+
+class UserSignUpForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+    email = forms.CharField(widget=forms.EmailInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+    password1 = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+    password2 = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
