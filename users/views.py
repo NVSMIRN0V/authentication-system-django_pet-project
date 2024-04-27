20da017bc9631baeb66f07980209845a8a882a88
Nikita Smirnov <job.nvsmirnov@gmail.com>
2024-04-27 10:53:33 +0300

    templates and static added
    Добавил шаблоны, которые позднее доработаю.

--- users/views.py
+++ users/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    return HttpResponse(f'Home page.')
+    return render(request, 'users/index.html', context={'title': 'Главная'})
 
 
 def signin(request: HttpRequest) -> HttpResponse:
-    return HttpResponse(f'Sign in page.')
+    return render(request, 'users/signin.html', context={'title': 'Войти'})
 
 
 def signup(request: HttpRequest) -> HttpResponse:
-    return HttpResponse(f'Sign up page.')
+    return render(request, 'users/signup.html', context={'title': 'Зарегистрироваться'})
 
 
 def signout(request: HttpRequest) -> HttpResponse:
-    return HttpResponse(f'Sign out page.')
+    return render(request, 'users/signout.html', context={'title': 'Выйти'})
